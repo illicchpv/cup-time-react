2688f44f500f86dc5977ca0bd0ca240e1a16b87f
illicchpv <illich58@mail.ru>
2024-08-01 13:11:39 +0300

    дз. как-то сделал. точности никакой.

--- src/products.js
+++ src/products.js
@@ -1,6 +1,6 @@
 
 export const products = [
-  {id: 1, in: true, title: "Кокосовая матча", price: 390, image: "./image/chai/photo-2.jpg"},
+  {id: 1, in: true, title: "матча Кокосовая матча Кокосовая матча матча", price: 390, image: "./image/chai/photo-2.jpg"},
   {id: 2, in: true, title: "Зеленый индонезийский чай", price: 340, image: "./image/chai/photo-1.jpg"},
   {id: 3, in: true, title: "Черный чай из Эфиопии", price: 250, image: "./image/chai/photo.jpg"},
   {id: 4, in: true, title: "Красный чай", price: 290, image: "./image/chai/photo-3.jpg"},
