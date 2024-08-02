76c643128e77109cc9ebf64ab1d5ac21372bb8cb
illicchpv <illich58@mail.ru>
2024-08-02 14:53:22 +0300

    выделение выбранной категории

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,6 +1,13 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 export const Header = () => {
+  const location = useLocation();
+
+  const getActiveClass = (category) => {
+    const currentCategory = new URLSearchParams(location.search).get("category");
+    return currentCategory === category ? "active" : "";
+  };
+
   return (
     <header className="header">
       <div className="container header__container">
@@ -11,19 +18,19 @@ export const Header = () => {
         <nav className="header__nav">
           <ul className="header__menu">
             <li className="header__menu-item">
-              <Link className="header__menu-link" to="/products?category=еуф">Чай</Link>
+              <Link className={`header__menu-link ${getActiveClass("tea")}`} to="/products?category=tea">Чай</Link>
             </li>
             <li className="header__menu-item">
-              <a className="header__menu-link" to="/products?category=coffee">Кофе</a>
+              <Link  className={`header__menu-link ${getActiveClass("coffee")}`} to="/products?category=coffee">Кофе</Link>
             </li>
             <li className="header__menu-item">
-              <Link className="header__menu-link" to="/products?category=teapots">Чайники</Link>
+              <Link className={`header__menu-link ${getActiveClass("teapots")}`} to="/products?category=teapots">Чайники</Link>
             </li>
             <li className="header__menu-item">
-              <a className="header__menu-link" to="/products?category=cezves">Турки</a>
+              <Link className={`header__menu-link ${getActiveClass("cezves")}`} to="/products?category=cezves">Турки</Link>
             </li>
             <li className="header__menu-item">
-              <Link className="header__menu-link" to="/products?category=other">Прочее</Link>
+              <Link className={`header__menu-link ${getActiveClass("other")}`} to="/products?category=other">Прочее</Link>
             </li>
           </ul>
         </nav>
