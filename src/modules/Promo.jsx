d78efc16b9eb23ee812c313c6d44ef6cff7d3a18
illicchpv <illich58@mail.ru>
2024-08-04 11:13:16 +0300

    Promo прячем кнопку для кофе

--- src/modules/Promo.jsx
+++ src/modules/Promo.jsx
@@ -1,13 +1,19 @@
-import {Link} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
 
 export const Promo = () => {
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
+
   return (
     <section className="promo">
       <div className="container">
         <div className="promo__container">
           <h1 className="promo__title">Попробуй новый вкус Арабики</h1>
 
-          <Link className="promo__link" to="/products?category=coffee">Перейти к кофе</Link>
+{category != "coffee" 
+  ? <Link className="promo__link" to="/products?category=coffee">Перейти к кофе</Link>
+  : <Link style={{visibility: "hidden"}} className="promo__link" to="/products?category=coffee">Перейти к кофе</Link>
+}
         </div>
       </div>
     </section>
